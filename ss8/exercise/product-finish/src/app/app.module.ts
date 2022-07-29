5bf9ee34f6f37da1da70c78ee73ff6da30adaa1d
quoctrungdn102 <trungtranfpt12345@gmail.com>
2022-07-29 09:10:07 +0700

    m

---
+++ ss8/exercise/product-finish/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
